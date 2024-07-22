78f98f8f5a30e7b4a9c4e407ac6d4ee15e3bb447
VitoMarino <marino.vito2001@gmail.com>
2024-07-22 23:11:03 +0200

    CSV

--- database/migrations/2024_07_22_121924_create_animal.php
+++ database/migrations/2024_07_22_121924_create_animal.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('animal', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->string('description', 50);
+            $table->string('description', 600);
             $table->string('species', 50);
-            $table->timestamps();
+            // $table->timestamps(null);
         });
     }
 
--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -14,12 +14,38 @@ class AnimalSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 100; $i++) {
-            $animal = new Animal();
-            $animal->name = $faker->name();
-            $animal->description = $faker->text(20);
-            $animal->species = $faker->text(10);
-            $animal->save();
+        $animal = $this->getCsvInfo(__DIR__ . '/animals.csv');
+
+        foreach ($animal as $index => $singleAnimal) {
+            if ($index > 0) {
+                $newAnimal = new Animal();
+                $newAnimal->name = $singleAnimal[1];
+                $newAnimal->description = $singleAnimal[2];
+                // $newAnimal->species = $singleAnimal[3];
+                // $newAnimal->timestamps = null;
+                $newAnimal->save();
+            }
         }
+    }
+
+    public function getCsvInfo($filePath)
+    {
+        $csvData = [];
+
+        // Apri il file, e lo devi leggere con 'r'
+        $fileData = fopen($filePath, 'r');
+
+        if ($fileData === false) {
+            throw new \InvalidArgumentException('File not found');
+        }
+
+        // Se lo trovi leggi riga per riga
+        while (($csvRow = fgetcsv($fileData)) !== false) {
+            $csvData[] = $csvRow;
+        }
+        // Chiudo il file
+        fclose($fileData);
+
+        return $csvData;
     }
 }
