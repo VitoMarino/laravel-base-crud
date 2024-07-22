6e0b58104ebb70bedba6a48176dfff54566e3dd3
VitoMarino <marino.vito2001@gmail.com>
2024-07-22 15:00:58 +0200

    Push

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
+    return view('pages.index');
 });
+Route::get('/animal', [PageController::class, 'index']);
+// Route::get('/animal{id}', [PageController::class, 'show']);
